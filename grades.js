cbc7d5975f68c9fb4c5384aa7165a310f973e245
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-04-06 17:30:46 +0300

    initial node assignment commit

---
+++ grades.js
@@ -0,0 +1,17 @@
+
+const grade = 80;
+if (grade >= 80) {
+    console.log("You have an A")
+} else if ((grade >= 65) && (grade < 80)) {
+    console.log("You have a B")
+} else if ((grade >= 50) && (grade < 65)) {
+    console.log("You passed")
+} else if ((grade < 50) && (grade >= 0)) {
+    console.log("You have failed!")
+} else  console.log("Grade should be between 0 to 100")
+
+
+
+
+
+
